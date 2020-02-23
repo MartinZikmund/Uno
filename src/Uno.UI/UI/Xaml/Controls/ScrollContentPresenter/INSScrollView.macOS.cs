98b26718dfb82fab9abfbd9d25526dbe7f03a6ee
Martin Zikmund <martin@zikmund.dev>
2020-02-23 22:05:42 +0100

    feat: Adding font-related extensions for macOS

---
+++ src/Uno.UI/UI/Xaml/Controls/ScrollContentPresenter/INSScrollView.macOS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreGraphics;
+
+namespace Windows.UI.Xaml.Controls
+{
+	internal interface INSScrollView
+	{
+		CGPoint UpperScrollLimit { get; }
+		void SetContentOffset(CGPoint contentOffset, bool animated);
+		void SetZoomScale(nfloat scale, bool animated);
+	}
+}
---
+++ src/Uno.UI/UI/Xaml/Controls/ScrollContentPresenter/IScrollContentPresenter.macOS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreGraphics;
+using AppKit;
+
+namespace Windows.UI.Xaml.Controls
+{
+	internal partial interface IScrollContentPresenter : INSScrollView
+	{
+	}
+}
