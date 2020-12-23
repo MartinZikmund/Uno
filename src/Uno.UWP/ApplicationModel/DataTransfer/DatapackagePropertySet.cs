c6cf192b0d1351bb036249fd83c3a2ca38c480f5
Martin Zikmund <martin@zikmund.dev>
2020-12-23 14:04:16 +0100

    feat: DataTransferManager support on WASM
    - Support for DataTransferManager sharing text/url data on WASM
    - IsSupported method added to all targets

---
+++ src/Uno.UWP/ApplicationModel/DataTransfer/DatapackagePropertySet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Windows.ApplicationModel.DataTransfer
+{
+	public partial class DataPackagePropertySet : IDictionary<string, object>, IEnumerable<KeyValuePair<string, object>>
+	{
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+	}
+}
--- src/Uno.UWP/Generated/3.0.0.0/Windows.ApplicationModel.DataTransfer/DataPackagePropertySet.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.ApplicationModel.DataTransfer/DataPackagePropertySet.cs
@@ -2,12 +2,12 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.ApplicationModel.DataTransfer
 {
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+	#if false
 	[global::Uno.NotImplemented]
 	#endif
 	public  partial class DataPackagePropertySet : global::System.Collections.Generic.IDictionary<string, object>,global::System.Collections.Generic.IEnumerable<global::System.Collections.Generic.KeyValuePair<string, object>>
 	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+		#if false
 		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public  string Title
 		{
@@ -35,7 +35,7 @@ namespace Windows.ApplicationModel.DataTransfer
 			}
 		}
 		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+		#if false
 		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public  string Description
 		{
