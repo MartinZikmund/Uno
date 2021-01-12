ebbbac93b76e1da8eb3b85629f38221276ad5389
Martin Zikmund <martin@zikmund.dev>
2021-01-12 02:02:29 +0100

    test: Repro for #4881

---
+++ src/SamplesApp/UITests.Shared/Resources/ResourcesInTemplate/SimpleControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Resources.ResourcesInTemplate
+{
+	public sealed partial class SimpleControl : Control
+	{
+		public SimpleControl()
+		{
+			this.DefaultStyleKey = typeof(SimpleControl);
+		}
+	}
+}
