7973c57198fcbc5bf5771fbb76e3b9041011982c
Martin Zikmund <martin@zikmund.dev>
2021-01-17 09:51:51 +0100

    test: TextBox/TextBlock selection

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBlockControl/TextBlock_Selection.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.TextBlockControl
+{
+	[Sample]
+    public sealed partial class TextBlock_Selection : Page
+    {
+        public TextBlock_Selection()
+        {
+            this.InitializeComponent();
+        }
+
+		private void SelectAll_Click(object sender, RoutedEventArgs e)
+		{
+			Text.SelectAll();
+			Text.Focus(FocusState.Programmatic);
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBox/TextBox_SelectionMethods.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.TextBox
+{
+	[Sample]
+	public sealed partial class TextBox_SelectionMethods : Page
+    {
+        public TextBox_SelectionMethods()
+        {
+            this.InitializeComponent();
+        }
+
+		private void SelectAll_Click(object sender, RoutedEventArgs e)
+		{
+			Text.SelectAll();
+			Text.Focus(FocusState.Programmatic);
+		}
+	}
+}
