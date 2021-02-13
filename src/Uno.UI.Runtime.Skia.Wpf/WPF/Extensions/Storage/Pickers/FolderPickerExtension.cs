e2a73fba2755d16daa266adb1a196b18a82d72f7
Martin Zikmund <martin@zikmund.dev>
2021-02-13 22:23:53 +0100

    chore: Started Skia WPF FolderPicker

---
+++ src/Uno.UI.Runtime.Skia.Wpf/WPF/Extensions/Storage/Pickers/FolderPickerExtension.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Uno.Extensions.Storage.Pickers
+{
+	internal class FolderPickerExtension : IFolderPickerExtension
+	{
+		private readonly FolderPicker _picker;
+
+		public FolderPickerExtension(object owner)
+		{
+			if (!(owner is FolderPicker picker))
+			{
+				throw new InvalidOperationException("Owner of FolderPickerExtension must be a FolderPicker.");
+			}
+			_picker = picker;
+		}
+
+		public async Task<StorageFolder?> PickSingleFolderAsync(CancellationToken token)
+		{
+			using var dialog = new FolderBrowserDialog();
+			dialog.ShowNewFolderButton = true;
+			dialog.RootFolder = PickerHelpers.GetInitialSpecialFolder(_picker.SuggestedStartLocation);
+
+			var result = dialog.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				return await StorageFolder.GetFolderFromPathAsync(dialog.SelectedPath);
+			}
+			return null;
+		}
+	}
+}
