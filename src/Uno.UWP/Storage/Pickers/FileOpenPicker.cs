92393d144051b3c2d68b9fbaf1587ce80a1d1839
Martin Zikmund <martin@zikmund.dev>
2021-03-01 21:49:22 +0100

    feat: iOS FileOpenPicker

--- src/Uno.UWP/Storage/Pickers/FileOpenPicker.cs
+++ src/Uno.UWP/Storage/Pickers/FileOpenPicker.cs
@@ -47,7 +47,7 @@ namespace Windows.Storage.Pickers
 			set => _commitButtonText = value ?? throw new ArgumentNullException(nameof(value));
 		}
 
-#if __SKIA__ || __WASM__
+#if __SKIA__ || __WASM__ || __IOS__
 		public FileOpenPicker()
 		{
 			InitializePlatform();
