661e73687e5f3cb101746448255417bf304e1ffa
Martin Zikmund <martin@zikmund.dev>
2021-04-21 12:02:38 +0200

    test(Focus): FocusState.Cycle sample page

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/FocusManager/Focus_FocusCycle.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml.FocusManager
+{
+	[Sample]
+    public sealed partial class Focus_FocusCycle : Page
+    {
+        public Focus_FocusCycle()
+        {
+            InitializeComponent();
+        }
+
+		private void FocusFirstClick(object sender, RoutedEventArgs args)
+		{
+			B1.Focus(FocusState.Keyboard);
+		}
+
+		private async void FocusNextClick(object sender, RoutedEventArgs args)
+		{
+			var nextElement = Windows.UI.Xaml.Input.FocusManager.FindNextElement(Windows.UI.Xaml.Input.FocusNavigationDirection.Next);
+			await Windows.UI.Xaml.Input.FocusManager.TryFocusAsync(nextElement, FocusState.Keyboard);
+		}
+
+		private async void FocusPreviousClick(object sender, RoutedEventArgs args)
+		{
+			var previousElement = Windows.UI.Xaml.Input.FocusManager.FindNextElement(Windows.UI.Xaml.Input.FocusNavigationDirection.Previous);
+			await Windows.UI.Xaml.Input.FocusManager.TryFocusAsync(previousElement, FocusState.Keyboard);
+		}
+	}
+}
