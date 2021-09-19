069508c1c12fda310ffbee7975758aa8e70f7ecb
Martin Zikmund <martin@zikmund.dev>
2021-09-19 16:16:24 +0200

    chore: Start updates

---
+++ src/Uno.UWP/Devices/Enumeration/Internal/Providers/Sensors/AccelerometerDeviceClassProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.Devices.Enumeration.Internal;
+
+namespace Windows.Devices.Enumeration.Internal.Providers.Sensors
+{
+	internal class AccelerometerDeviceClassProvider : IDeviceClassProvider
+	{
+		public bool CanWatch => throw new NotImplementedException();
+
+		public event EventHandler<DeviceInformation> WatchAdded;
+		public event EventHandler<DeviceInformation> WatchEnumerationCompleted;
+		public event EventHandler<DeviceInformationUpdate> WatchRemoved;
+		public event EventHandler<object> WatchStopped;
+		public event EventHandler<DeviceInformationUpdate> WatchUpdated;
+
+		public Task<DeviceInformation[]> FindAllAsync() => throw new NotImplementedException();
+		public void WatchStart() => throw new NotImplementedException();
+		public void WatchStop() => 
+	}
+}
