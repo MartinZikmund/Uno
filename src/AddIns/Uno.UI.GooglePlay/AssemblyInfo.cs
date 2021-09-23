c09c7db1417ceb6f87fb2e033382a43b6aabd925
Martin Zikmund <martin@zikmund.dev>
2021-09-23 22:43:42 +0200

    feat: In-app rating request on Android

---
+++ src/AddIns/Uno.UI.GooglePlay/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using Windows.UI.ViewManagement;
+using Uno.Devices.Sensors;
+using Uno.Foundation.Extensibility;
+
+[assembly: ApiExtension(typeof(IApplicationViewSpanningRects), typeof(DuoApplicationViewSpanningRects))]
+[assembly: ApiExtension(typeof(INativeHingeAngleSensor), typeof(DuoHingeAngleSensor))]
---
+++ src/AddIns/Uno.UI.GooglePlay/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Google.Android.Play.Core.Review;
+
+namespace Uno.UI.GooglePlay
+{
+    public class Class1
+    {
+		public void Main()
+		{
+			var fact = ReviewManagerFactory.Create(ContextHelper.Current);
+			fact.
+		}
+    }
+}
