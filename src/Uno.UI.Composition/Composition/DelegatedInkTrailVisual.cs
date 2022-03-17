4f8daf369cbae2d4f41b0c137b40c250a569ca0d
Martin Zikmund <martin@zikmund.dev>
2022-03-17 23:41:44 +0100

    feat: Update API surface to match Windows 11 22000 SDK

---
+++ src/Uno.UI.Composition/Composition/DelegatedInkTrailVisual.cs
@@ -0,0 +1,10 @@
+﻿namespace Windows.UI.Composition
+{	
+	public partial class DelegatedInkTrailVisual : Visual
+	{
+		// Required constructor for base class
+		private DelegatedInkTrailVisual(Compositor compositor) : base(compositor)
+		{
+		}
+	}
+}
--- src/Uno.UWPSyncGenerator/Generator.cs
+++ src/Uno.UWPSyncGenerator/Generator.cs
@@ -124,6 +124,25 @@ namespace Uno.UWPSyncGenerator
 #endif
 			};
 
+			List<string> excludeNamespaces = new List<string>();
+			List<string> includeNamespaces = new List<string>();
+#if !HAS_UNO_WINUI
+			// For UWP compilation we need to ignore these namespaces when not explicitly generating
+			// for related projects.
+			excludeNamespaces.Add("Windows.UI.Dispatching");
+			excludeNamespaces.Add("Windows.UI.Composition");
+			if (baseName == "Uno.UI.Dispatching")
+			{
+				excludeNamespaces.Clear();
+				includeNamespaces.Add("Windows.UI.Dispatching");
+			}
+			if (baseName == "Uno.UI.Composition")
+			{
+				excludeNamespaces.Clear();
+				includeNamespaces.Add("Windows.UI.Composition");
+			}
+#endif
+
 			var q = from asm in origins
 					where asm.Name == sourceAssembly
 					from targetType in GetNamespaceTypes(asm.Modules.First().GlobalNamespace)
@@ -134,15 +153,8 @@ namespace Uno.UWPSyncGenerator
 						(baseName == "Uno.UI" || baseName == "Uno.UI.Dispatching" || baseName == "Uno.UI.Composition")
 						&& unoUINamespaces.Any(n => targetType.ContainingNamespace.ToString().StartsWith(n))
 					)
-#if !HAS_UNO_WINUI
-					where (
-						(baseName != "Uno.UI.Composition" && baseName != "Uno.UI.Dispatching") ||
-						(
-							(targetType.ContainingNamespace.ToString().StartsWith("Windows.UI.Composition") && baseName == "Uno.UI.Composition") ||
-							(targetType.ContainingNamespace.ToString().StartsWith("Windows.UI.Dispatching") && baseName == "Uno.UI.Dispatching")
-						)
-					)
-#endif
+					where !excludeNamespaces.Any(n => targetType.ContainingNamespace.ToString().StartsWith(n))
+					where includeNamespaces.Any(n => targetType.ContainingNamespace.ToString().StartsWith(n))
 					group targetType by targetType.ContainingNamespace into namespaces
 					orderby namespaces.Key.MetadataName
 					select new
