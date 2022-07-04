bf6d601d5ae833ff6058f38cc6c8d507e22bacee
Martin Zikmund <martin@zikmund.dev>
2022-07-04 14:36:54 +0200

    chore: Include StoreContract in ApiInformation

--- src/Uno.Foundation/Metadata/ApiInformation.shared.cs
+++ src/Uno.Foundation/Metadata/ApiInformation.shared.cs
@@ -27,6 +27,10 @@ public partial class ApiInformation
 				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.Networking.Connectivity.WwanContract
 				return majorVersion <= 2; // SDK 10.0.22000.0
 
+			case "Windows.Services.Store.StoreContract":
+				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.Services.Store.StoreContract
+				return majorVersion <= 4; // SDK 10.0.22000.0
+
 			case "Windows.ApplicationModel.Calls.CallsPhoneContract":
 				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.ApplicationModel.Calls.CallsPhoneContract
 				return majorVersion <= 6; // SDK 10.0.22000.0
